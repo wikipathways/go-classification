546991a2b8574ae9071dd0c8b9eb5b7c42254541
mkutmon <mkutmon@gmail.com>
2013-05-03 12:43:08 +0200

    perl script to format output files for wiki/trac page

---
+++ results-wiki-page-formatting.pl
@@ -0,0 +1,82 @@
+use strict;
+use warnings;
+
+my $dir = '/home/martina/bigcat/projects/wikipathways/go-annotations/output/';
+open (my $OUTPUT, '>>/home/martina/bigcat/projects/wikipathways/go-annotations/wiki.txt');
+my $sum = 0;
+my$count = 0;
+
+foreach my $DIR (glob("$dir/*.txt")) 
+{
+    # print $DIR . "\n";
+     
+     open my $FILE, "<", $DIR or die "can't read open '$DIR'";
+     my $countGO = 0;
+     my $count = 1;
+     
+     while(<$FILE>)
+     {
+          
+          
+          if($count == 1) {
+                 my $line = $_;
+                 chomp $line;
+                 
+                  my @values = split(' ', $line);
+                  my $pwId = substr($values[1], 6, (length($values[1])-10));
+                  my $l  = scalar @values;
+                  my $genecount = $values[$l-1];
+                  my $link = "http://wikipathways.org/index.php/Pathway:" . $pwId;
+                  my $name = "";
+                  for (my $i = 2; $i <= $l-2 ; $i++) {
+                       $name = $name . " " . $values[$i];
+                   }
+                   
+                  print $pwId .  "\t" . $name . "\t" .  $genecount . "\n" . $link . "\n";
+                  
+                  print $OUTPUT "=== " . $name . " ===\n\n";
+                  print $OUTPUT $genecount . " genes, [$link pathway] \n\n";
+                  print $OUTPUT "||'''GO term'''||'''GO ID'''||'''changed'''||'''total'''||'''zcore'''||'''weighted'''||\n";
+#=== Glutathione metabolism ===
+
+#20 genes[[BR]]
+#link
+
+#||glutathione metabolic process||GO:0006749||15||37||305.56288314231733||[link link]||
+	 
+
+#----
+                  
+           }
+           if($count > 2) {
+                if($countGO < 15) {
+                    my $line = $_;
+                    chomp $line;
+                 
+                    my @values = split('\t', $line);
+                    my $goid = $values[1];
+                    #$goid =~ tr/:/_/;
+                    
+                    my $link =  "http://purl.obolibrary.org/obo/" . $goid;
+                    my $term = $values[0];
+                    my $changed  = $values[2];
+                    my $total = $values[4];
+                    my $weightedzscore = substr($values[6], 0, 5);
+                    my $zscore = substr($values[5], 0, 5);
+                    print $term . "\t" . $changed . "\t" . $total . "\t" . $weightedzscore . "\n";
+                    print $link . "\n";
+                    if($total < 2000) {
+                         print $OUTPUT "||$term||$goid||$changed||$total||$zscore||$weightedzscore||\n";
+                           $countGO++;
+                    }
+               }
+            }
+           $count++;
+      }
+      
+       print $OUTPUT "\n----\n";
+     
+     close $FILE;
+ }
+ 
+ close ($OUTPUT); 
